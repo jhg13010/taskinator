f58a1b4c87ebeb2b124f56e7d100becf3dc12119
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-03 16:29:14 -0600

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
+//connect js to HTML forms and ul
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+//create functin to handle form submission
 var createTaskHandler = function(event) {
+    //stop page refresh 
     event.preventDefault();
+    
+    //connect js to form inputs
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+    
+    //create list elements 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     
-
+    //create div element within list element to enable 2 headings 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
+
+    //apply html content to div element to enable 2 headings 
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
 
+    //append div to li and li to ul
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-
+//call form on form submission (click or enter)
 formEl.addEventListener("submit", createTaskHandler);
 
 /*feature/form-submit
