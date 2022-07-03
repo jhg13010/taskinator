5f2d0a13576d409ac58e75426066253af182d111
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-03 16:37:55 -0600

    refactored code to breakout collection and appendation into two functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //create functin to handle form submission
-var createTaskHandler = function(event) {
+
+function taskFormHandler(event) {
     //stop page refresh 
     event.preventDefault();
     
     //connect js to form inputs
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    //convert data to an object 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput,
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+function createTaskEl(taskDataObj) {
     //create list elements 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -20,15 +31,19 @@ var createTaskHandler = function(event) {
     taskInfoEl.className = "task-info";
 
     //apply html content to div element to enable 2 headings 
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     //append div to li and li to ul
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
-};
+}
+
 
 //call form on form submission (click or enter)
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
+
+
+
 
 /*feature/form-submit
     - create task via form and indicate type of task
@@ -38,7 +53,7 @@ To Do:
     - create feature ==> DONE
     - add HTML ==> DONE
     - handle form submission ==> DONE
-    - capture form values
+    - capture form values ==> DONE
     - organize functionality
     - address usability
     - save with Git 
