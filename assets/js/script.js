f3520183578b334ed064daf28e267b2f569f1c4c
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-03 16:25:50 -0600

    fixed errors in HTML, restructured createTaskHandler to take both inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var taskInput = document.querySelector("#task-name");
 
-/* function getTask(callback) {
-    var task = prompt("Enter Task Name");
-    callback(addTask);
-} */
-
-function createTaskHandler(event) {
+var createTaskHandler = function(event) {
     event.preventDefault();
-    var listItemEL = document.createElement("li");
-    listItemEL.className = "task-item";
-    listItemEL.textContent = "test"; //taskInput.value;
-    tasksToDoEl.appendChild(listItemEL);
-}
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-formEl.addEventListener("click", createTaskHandler) 
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+
+formEl.addEventListener("submit", createTaskHandler);
 
 /*feature/form-submit
     - create task via form and indicate type of task
@@ -24,7 +28,7 @@ formEl.addEventListener("click", createTaskHandler)
 To Do: 
     - create feature ==> DONE
     - add HTML ==> DONE
-    - handle form submission 
+    - handle form submission ==> DONE
     - capture form values
     - organize functionality
     - address usability
