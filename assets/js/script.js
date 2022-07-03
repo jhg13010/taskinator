da0bdddeccfa0d0e5329d4d490f1a7cd8551e1c5
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-03 15:40:18 -0600

    structured js to preventDefault and take form submissions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskInput = document.querySelector("#task-name");
 
 /* function getTask(callback) {
     var task = prompt("Enter Task Name");
     callback(addTask);
 } */
 
-function addTask() {
+function createTaskHandler(event) {
+    event.preventDefault();
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "new task";
+    listItemEL.textContent = "test"; //taskInput.value;
     tasksToDoEl.appendChild(listItemEL);
 }
 
-buttonEl.addEventListener("click", addTask) 
+formEl.addEventListener("click", createTaskHandler) 
 
 /*feature/form-submit
     - create task via form and indicate type of task
     - submit form via button or enter 
     - prevent empty form fields from submitting 
 To Do: 
-    - create feature 
-    - add HTML 
+    - create feature ==> DONE
+    - add HTML ==> DONE
     - handle form submission 
     - capture form values
     - organize functionality
