f11a04b35a4fa2b7f06866332a2026b15522237f
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-03 14:57:52 -0600

    added function to add new task to the to do list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+/* function getTask(callback) {
+    var task = prompt("Enter Task Name");
+    callback(addTask);
+} */
+
+function addTask() {
+    var listItemEL = document.createElement("li");
+    listItemEL.className = "task-item";
+    listItemEL.textContent = "new task";
+    tasksToDoEl.appendChild(listItemEL);
+}
+
+buttonEl.addEventListener("click", addTask) 
